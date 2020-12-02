3fbcd7542dcc09bbb9a3fb98b6af27a68d56cd2e
Lei Shi <me@leishi.io>
2020-12-02 11:35:00 -0800

    Bigger pool size

--- app/db/session.py
+++ app/db/session.py
@@ -6,6 +6,7 @@ from app.core import config
 
 engine = create_engine(
     config.SQLALCHEMY_DATABASE_URI,
+    pool_size=20, max_overflow=0
 )
 SessionLocal: Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
