263e059c06c4b4ceed37528960a27dad6f4a160d
Smart Catboy <smartcatboy@tuta.io>
2021-12-24 15:02:13 +0800

    fix: dns query timeout

--- app/utils/dns.py
+++ app/utils/dns.py
@@ -1,5 +1,6 @@
 import json
 import traceback
+from urllib.error import URLError
 from urllib.parse import urlencode
 from urllib.request import Request, urlopen
 
@@ -12,7 +13,11 @@ def dns_query(url: str, doh_url: str = DEFAULT_DOH_URL) -> str:
     req = Request(
         f"{doh_url}?{params}", headers={"accept": "application/dns-json"}
     )
-    result = json.loads(urlopen(req, timeout=2).read().decode("utf-8"))
+    try:
+        ret = urlopen(req, timeout=2).read().decode("utf-8")
+    except URLError:
+        return url
+    result = json.loads(ret)
     if not result.get("Answer"):
         return url
     return result.get("Answer", [])[-1]["data"]
