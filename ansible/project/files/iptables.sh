f690201431cb7c0ae98d65f6bff366088f3d4385
smartcatboy <smartcatboy@tuta.io>
2022-01-06 18:17:59 +0800

    refactor: iptables

--- ansible/project/files/iptables.sh
+++ ansible/project/files/iptables.sh
@@ -8,36 +8,121 @@ LOCAL_PORT=0
 REMOTE_IP=0
 REMOTE_PORT=0
 
+check_system () {
+    source '/etc/os-release'
+    if [ $ID = "centos" ]; then
+        OS_FAMILY="centos"
+        UPDATE="$SUDO yum makecache -y"
+        INSTALL="$SUDO yum install -y iptables"
+    elif  [ $ID = "debian" ] || [ $ID = "ubuntu" ]; then
+        OS_FAMILY="debian"
+        UPDATE="$SUDO apt update -y"
+        INSTALL="$SUDO apt install -y iptables"
+    else
+        echo -e "System $ID ${VERSION_ID} is not supported now"
+        exit 1
+    fi
+}
+
+install_ipt () {
+    iptables -V || $INSTALL || ($UPDATE && $INSTALL)
+    if [ $? -ne 0 ]; then
+        echo -e "Failed to install iptables"
+        exit 1
+    fi
+}
+
+disable_firewall () {
+    if [ $OS_FAMILY = "centos" ]; then
+        if [[ -n $(systemctl list-unit-files --all | grep "firewalld.service" | grep "enabled") ]]; then
+            $SUDO systemctl stop firewalld.service && \
+            $SUDO systemctl disable firewalld.service
+	fi
+    else
+        if [[ -n $(systemctl list-unit-files --all | grep "ufw.service" | grep "enabled") ]]; then
+            $SUDO systemctl stop ufw.service && \
+            $SUDO systemctl disable ufw.service
+	fi
+    fi
+}
+
+check_ipt_restore_file () {
+    if [ $OS_FAMILY = "centos" ]; then
+        IPT_PATH="/etc/sysconfig"
+    else
+        IPT_PATH="/etc/iptables"
+    fi
+    if [ ! -d $IPT_PATH ]; then
+        $SUDO mkdir -p $IPT_PATH
+    fi
+    if [ $OS_FAMILY = "centos" ]; then
+        IPT_RESTORE_FILE=$IPT_PATH/iptables
+    else
+        IPT_RESTORE_FILE=$IPT_PATH/rules.v4
+    fi
+    if [ ! -f $IPT_RESTORE_FILE ]; then
+        $SUDO touch $IPT_RESTORE_FILE
+    fi
+}
+
+install_ipt_service () {
+    IPT_SERVICE_PATH="/etc/systemd/system/iptables-restore.service"
+    if [ ! -f $IPT_SERVICE_PATH ]; then
+        $SUDO touch $IPT_SERVICE_PATH
+    fi
+    if [ $OS_FAMILY = "centos" ]; then
+        $SUDO cat > $IPT_SERVICE_PATH <<EOF
+[Unit]
+Description=Restore iptables rule by Aurora Admin Panel
+
+[Service]
+Type=oneshot
+ExecStart=/bin/sh -c '/usr/sbin/iptables-restore -c < /etc/sysconfig/iptables'
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    else
+        $SUDO cat > $IPT_SERVICE_PATH <<EOF
+[Unit]
+Description=Restore iptables rule by Aurora Admin Panel
+
+[Service]
+Type=oneshot
+ExecStart=/bin/sh -c '/usr/sbin/iptables-restore -c < /etc/iptables/rules.v4'
+
+[Install]
+WantedBy=multi-user.target
+EOF
+    fi
+    check_ipt_restore_file
+}
+
+check_ipt_service () {
+    IPT_SERVICE="iptables-restore.service"
+    if [[ -z $(systemctl list-unit-files --all | grep "$IPT_SERVICE" | grep "enabled") ]]; then
+        install_ipt_service
+    fi
+    $SUDO systemctl daemon-reload && \
+    $SUDO systemctl enable $IPT_SERVICE
+    if [ $? -ne 0 ]; then
+        echo -e "Failed to install iptables restore service"
+        exit 1
+    fi
+}
+
 save_iptables () {
-  if [[ -f /etc/redhat-release ]]; then
-    release="centos"
-  elif cat /etc/issue | grep -q -E -i "debian"; then
-    release="debian"
-  elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-    release="ubuntu"
-  elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-    release="centos"
-  elif cat /proc/version | grep -q -E -i "debian"; then
-    release="debian"
-  elif cat /proc/version | grep -q -E -i "ubuntu"; then
-    release="ubuntu"
-  elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-    release="centos"
-  fi
-  if [ $release = "centos" ]; then
-    $SUDO mkdir -p /etc/sysconfig
-    $SUDO iptables-save -c | $SUDO tee /etc/sysconfig/iptables > /dev/null
-  else
-    $SUDO mkdir -p /etc/iptables
-    $SUDO iptables-save -c | $SUDO tee /etc/iptables/rules.v4 > /dev/null
-  fi
+    check_ipt_restore_file
+    $SUDO iptables-save -c | $SUDO tee $IPT_RESTORE_FILE > /dev/null
 }
 
 set_forward () {
-    if [[ -z $($SUDO cat /etc/sysctl.conf | grep 'net.ipv4.ip_forward = 1') ]]; then
+    if [[ -z $($SUDO cat /etc/sysctl.conf | grep "net.ipv4.ip_forward") ]]; then
         echo "net.ipv4.ip_forward = 1" | $SUDO tee -a /etc/sysctl.conf > /dev/null
-        $SUDO sysctl -p
+    elif [[ -n $($SUDO cat /etc/sysctl.conf | grep "net.ipv4.ip_forward" | grep "0") ]]; then
+        sed -i "s/.*net.ipv4.ip_forward.*/net.ipv4.ip_forward = 1/g" /etc/sysctl.conf > /dev/null
     fi
+    $SUDO sysctl -p
 }
 
 forward () {
@@ -88,9 +173,9 @@ list_all () {
 
 reset () {
    COMMENT="$LOCAL_PORT->" 
-   $SUDO iptables -L INPUT --line-numbers | grep $COMMENT | awk '{print $1}' | xargs -I{} sudo iptables -Z INPUT {}
-   $SUDO iptables -L OUTPUT --line-numbers | grep $COMMENT | awk '{print $1}' | xargs -I{} sudo iptables -Z OUTPUT {}
-   $SUDO iptables -L FORWARD --line-numbers | grep $COMMENT | awk '{print $1}' | xargs -I{} sudo iptables -Z FORWARD {}
+   $SUDO iptables -L INPUT --line-numbers | grep $COMMENT | awk '{print $1}' | xargs -I{} $SUDO iptables -Z INPUT {}
+   $SUDO iptables -L OUTPUT --line-numbers | grep $COMMENT | awk '{print $1}' | xargs -I{} $SUDO iptables -Z OUTPUT {}
+   $SUDO iptables -L FORWARD --line-numbers | grep $COMMENT | awk '{print $1}' | xargs -I{} $SUDO iptables -Z FORWARD {}
 }
 
 delete () {
@@ -135,6 +220,10 @@ then
     echo "Unknow remote port for operation $OPERATION" && exit 1
 fi
 
+check_system
+install_ipt
+disable_firewall
+check_ipt_service
 if [ $OPERATION == "forward" ]; then
     list
     delete
