6b9b367bf3a926c104ed155ceb434eb0438111e0
smartcatboy <smartcatboy@tuta.io>
2022-02-05 15:32:56 +0800

    feat: update clean script

--- ansible/project/files/clean.sh
+++ ansible/project/files/clean.sh
@@ -5,19 +5,23 @@ SUDO=$(if [ $(id -u $whoami) -gt 0 ]; then echo "sudo "; fi)
 clean_services () {
     [[ -z $1 ]] && return 0
     find /etc/systemd/system/multi-user.target.wants -maxdepth 1 -type l -name $1 -exec $SUDO systemctl disable --now {} +
+    find /etc/systemd/system/timers.target.wants -maxdepth 1 -type l -name $1 -exec $SUDO systemctl disable --now {} +
     find /etc/systemd/system -maxdepth 1 -type f -name $1 -exec $SUDO rm {} +
 }
 
 clean_iptables () {
-    while [[ ! -z "$($SUDO iptables -S | grep -Ee 'DOWNLOAD [0-9]+->' -Ee 'UPLOAD [0-9]+->' -Ee 'FORWARD [0-9]+->' -Ee 'BACKWARD [0-9]+->')" ]]
+    while [[ ! -z "$($SUDO iptables -S | grep -Ee 'DOWNLOAD(-UDP)? [0-9]+->' -Ee 'UPLOAD(-UDP)? [0-9]+->' -Ee 'FORWARD(-UDP)? [0-9]+->' -Ee 'BACKWARD(-UDP)? [0-9]+->')" ]]
     do
-    	$SUDO iptables -S | grep -Ee 'DOWNLOAD [0-9]+->' -Ee 'UPLOAD [0-9]+->' -Ee 'FORWARD [0-9]+->' -Ee 'BACKWARD [0-9]+->' | awk -v SUDO="$SUDO" '{$1="";$COMMEND=SUDO" iptables -D "$0; system($COMMEND)}'
+        $SUDO iptables -S | grep -Ee 'DOWNLOAD(-UDP)? [0-9]+->' -Ee 'UPLOAD(-UDP)? [0-9]+->' -Ee 'FORWARD(-UDP)? [0-9]+->' -Ee 'BACKWARD(-UDP)? [0-9]+->' | awk -v SUDO="$SUDO" '{$1="";$COMMEND=SUDO" iptables -D "$0; system($COMMEND)}'
     done
-    while [[ ! -z "$($SUDO iptables -t nat -S | grep -Ee 'DOWNLOAD [0-9]+->' -Ee 'UPLOAD [0-9]+->' -Ee 'FORWARD [0-9]+->' -Ee 'BACKWARD [0-9]+->')" ]]
+    while [[ ! -z "$($SUDO iptables -t nat -S | grep -Ee 'DOWNLOAD(-UDP)? [0-9]+->' -Ee 'UPLOAD(-UDP)? [0-9]+->' -Ee 'FORWARD(-UDP)? [0-9]+->' -Ee 'BACKWARD(-UDP)? [0-9]+->')" ]]
     do
-    	$SUDO iptables -t nat -S | grep -Ee 'DOWNLOAD [0-9]+->' -Ee 'UPLOAD [0-9]+->' -Ee 'FORWARD [0-9]+->' -Ee 'BACKWARD [0-9]+->' | awk -v SUDO="$SUDO" '{$1="";$COMMEND=SUDO" iptables -t nat -D "$0; system($COMMEND)}'
+        $SUDO iptables -t nat -S | grep -Ee 'DOWNLOAD(-UDP)? [0-9]+->' -Ee 'UPLOAD(-UDP)? [0-9]+->' -Ee 'FORWARD(-UDP)? [0-9]+->' -Ee 'BACKWARD(-UDP)? [0-9]+->' | awk -v SUDO="$SUDO" '{$1="";$COMMEND=SUDO" iptables -t nat -D "$0; system($COMMEND)}'
     done
     clean_services "iptables-restore.service"
+    clean_services "ip6tables-restore.service"
+    clean_services "iptables-check.service"
+    clean_services "iptables-check.timer"
 }
 
 clean_aurora () {
