6db68cbf6ecca9d831126bc71e95ecc9cbbe87ee
smartcatboy <smartcatboy@tuta.io>
2022-02-11 15:34:50 +0800

    feat: add xray core

--- tasks/functions/v2ray.py
+++ tasks/functions/v2ray.py
@@ -16,7 +16,6 @@ class V2rayConfig(AppConfig):
 
         self.app_name = "v2ray"
         self.app_path = "/usr/local/bin/"
-        self.app_version_arg = "-version"
 
         self.app_get_role_name = "v2ray_get"
         self.app_sync_role_name = "v2ray_sync"
@@ -30,7 +29,13 @@ class V2rayConfig(AppConfig):
         v2ray_config = generate_v2ray_config(port.forward_rule)
         with open(f"ansible/project/roles/app/files/v2ray-{port.id}", "w") as f:
             f.write(json.dumps(v2ray_config, indent=2))
-        self.app_command = f"/usr/local/bin/v2ray -config /usr/local/etc/aurora/{port.num}"
+        if not (core := port.forward_rule.config.get("core", "v2ray")):
+            core = "v2ray"
+        self.app_command = (
+            f"/usr/local/bin/{core} "
+            f"{'-format=json' if core == 'xray' else ''} "
+            f"-config /usr/local/etc/aurora/{port.num}"
+        )
         self.app_config = f"v2ray-{port.id}"
 
         self.update_app = not port.server.config.get("v2ray")
