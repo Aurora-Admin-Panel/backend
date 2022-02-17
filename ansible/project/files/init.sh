2dd8e5f5a660fd518d26a52f1fa2d9de64b255d3
smartcatboy <smartcatboy@tuta.io>
2022-02-17 20:38:10 +0800

    feat: optimize server init script

--- ansible/project/files/init.sh
+++ ansible/project/files/init.sh
@@ -1,29 +1,35 @@
-#! /bin/bash
-
-SUDO=$(if [ $(id -u $whoami) -gt 0 ]; then echo "sudo "; fi)
-[ -z $SUDO ] || sudo -n true 2>/dev/null || (echo "Failed to use sudo" && exit 1)
+#! /bin/sh
 
 check_system () {
-    source '/etc/os-release'
-    if [[ $ID == "centos" ]]; then
+    [ ! -f /etc/os-release ] && return 0
+    source /etc/os-release
+    if [ $ID = "centos" ]; then
         OS_FAMILY="centos"
-        UPDATE="$SUDO yum makecache -y"
-        INSTALL="$SUDO yum install -y"
-    elif [[ $ID == "debian" || $ID == "ubuntu" ]]; then
+        UPDATE="yum makecache -y"
+        INSTALL="yum install -y"
+    elif [ $ID = "debian" ] || [ $ID = "ubuntu" ]; then
         OS_FAMILY="debian"
-        UPDATE="$SUDO apt update -y"
-        INSTALL="$SUDO apt install -y --no-install-recommends"
-    elif [[ $ID == "alpine" ]]; then
+        UPDATE="apt update -y"
+        INSTALL="apt install -y --no-install-recommends"
+    elif [ $ID = "alpine" ]; then
         OS_FAMILY="alpine"
-        UPDATE="$SUDO apk update"
-        INSTALL="$SUDO apk add --no-cache"
+        UPDATE="apk update"
+        INSTALL="apk add --no-cache"
     fi
+}
+
+install_bash () {
+    echo "Checking bash ..."
+    bash --version > /dev/null 2>&1 && return 0
+    [ -z $OS_FAMILY ] && return 0
+    echo "Installing bash ..."
+    $INSTALL bash || ($UPDATE && $INSTALL bash) || (echo "Failed to install bash!" && return 1)
 }
 
 install_python3 () {
     echo "Checking python3 ..."
     python3 -V > /dev/null 2>&1 && return 0
-    [[ -z $OS_FAMILY ]] && return 0
+    [ -z $OS_FAMILY ] && return 0
     echo "Installing python3 ..."
     $INSTALL python3 || ($UPDATE && $INSTALL python3) || (echo "Failed to install python3!" && return 1)
 }
@@ -31,11 +37,11 @@ install_python3 () {
 install_python2 () {
     echo "Checking python2 ..."
     (python -V > /dev/null 2>&1 || python2 -V > /dev/null 2>&1) && return 0
-    [[ -z $OS_FAMILY ]] && return 0
+    [ -z $OS_FAMILY ] && return 0
     echo "Installing python2 ..."
-    if [[ $OS_FAMILY == "centos" || $OS_FAMILY == "debian" ]]; then
+    if [ $OS_FAMILY = "centos" ] || [ $OS_FAMILY = "debian" ]; then
         $INSTALL python || ($UPDATE && $INSTALL python) || echo "Failed to install python2!"
-    elif [[ $OS_FAMILY == "alpine" ]]; then
+    elif [ $OS_FAMILY = "alpine" ]; then
         $INSTALL python2 || ($UPDATE && $INSTALL python2) || echo "Failed to install python2!"
     fi
 }
@@ -43,7 +49,7 @@ install_python2 () {
 install_iptables () {
     echo "Checking iptables ..."
     iptables -V > /dev/null 2>&1 && return 0
-    [[ -z $OS_FAMILY ]] && return 0
+    [ -z $OS_FAMILY ] && return 0
     echo "Installing iptables ..."
     $INSTALL iptables || ($UPDATE && $INSTALL iptables) || echo "Failed to install iptables!"
 }
@@ -52,9 +58,9 @@ install_iproute () {
     echo "Checking iproute ..."
     ip a > /dev/null 2>&1 && return 0
     echo "Installing iproute ..."
-    if [[ $OS_FAMILY == "centos" ]]; then
+    if [ $OS_FAMILY = "centos" ]; then
         $INSTALL iproute || ($UPDATE && $INSTALL iproute) || echo "Failed to install iproute!"
-    elif [[ $OS_FAMILY == "debian" ]]; then
+    elif [ $OS_FAMILY = "debian" ]; then
         $INSTALL iproute2 || ($UPDATE && $INSTALL iproute2) || echo "Failed to install iproute2!"
     fi
 }
@@ -62,25 +68,25 @@ install_iproute () {
 install_systemd () {
     echo "Checking systemd ..."
     systemctl --version > /dev/null 2>&1 && return 0
-    [[ $OS_FAMILY != "centos" && $OS_FAMILY != "debian" ]] && return 0
+    [ $OS_FAMILY != "centos" ] && [ $OS_FAMILY != "debian" ] && return 0
     echo "Installing systemd ..."
     $INSTALL systemd || ($UPDATE && $INSTALL systemd) || echo "Failed to install systemd!"
 }
 
 check_paths () {
     echo "Checking paths ..."
-    [[ ! -d /usr/local/bin ]] && $SUDO mkdir -p /usr/local/bin
-    systemctl --version > /dev/null 2>&1 || return 0
-    [[ ! -d /usr/lib/systemd/system ]] && $SUDO mkdir -p /usr/lib/systemd/system
-    [[ ! -d /etc/systemd/system ]] && $SUDO mkdir -p /etc/systemd/system
+    [ ! -d /usr/local/bin ] && mkdir -p /usr/local/bin
+    [ $OS_FAMILY != "centos" ] && [ $OS_FAMILY != "debian" ] && return 0
+    [ ! -d /usr/lib/systemd/system ] && mkdir -p /usr/lib/systemd/system
+    [ ! -d /etc/systemd/system ] && mkdir -p /etc/systemd/system
 }
 
 delete_service () {
-    [[ -z $1 ]] && return 0 || SERVICE=$1
+    [ -z $1 ] && return 0 || SERVICE=$1
     systemctl --version > /dev/null 2>&1 || return 0
     echo -e "Disabling $SERVICE ..."
-    systemctl is-active --quiet $SERVICE > /dev/null 2>&1 && $SUDO systemctl stop $SERVICE
-    systemctl is-enabled --quiet $SERVICE > /dev/null 2>&1 && $SUDO systemctl disable $SERVICE
+    systemctl is-active --quiet $SERVICE > /dev/null 2>&1 && systemctl stop $SERVICE
+    systemctl is-enabled --quiet $SERVICE > /dev/null 2>&1 && systemctl disable $SERVICE
 }
 
 disable_firewall () {
@@ -89,6 +95,7 @@ disable_firewall () {
 }
 
 install_deps () {
+    install_bash
     install_python3 || install_python2
     install_iptables
     install_iproute
