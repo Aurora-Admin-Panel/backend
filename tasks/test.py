0e63aa38ae63886ee883a4a15c8565f01ae9dc95
Lei Shi <me@leishi.io>
2023-11-03 14:52:33 +0100

    update .gitignore

--- tasks/test.py
+++ tasks/test.py
@@ -1,18 +1,21 @@
 import time
 import redis
+from huey.api import Task
 from app.core import config
+
 from .config import huey
+from tasks.utils.connection import connect
 
 
-@huey.task()
-def test_task():
-    r = redis.Redis(host='redis', port=6379, db=0)
-    count = 0
-    while True:
-        if count % 100 != 0:
-            r.publish(config.PUBSUB_PREFIX+'test', str(count))
-        else:
-            r.publish(config.PUBSUB_PREFIX+'test', config.PUBSUB_STOPWORDS)
-        count += 1
-        print(count)
-        time.sleep(1)
+@huey.task(priority=3, context=True)
+def test_runner(server_id: int, task: Task):
+    try:
+        with connect(server_id=server_id, task=task) as c:
+            c.run("echo 1")
+            c.run("echo 2")
+            c.run("echo 3")
+            c.run("cat /etc/os-release")
+            return {"success": True}
+    except Exception as e:
+        # TODO: handle exception
+        return {"error": str(e)}
